5ac6fec287ea4603f3a54a465a8ed75ca9655b01
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-03 15:47:34 +1200

    Add Racer model

---
+++ app/models/racer.rb
@@ -0,0 +1,11 @@
+class Racer
+  # convenience method for access to client in console
+  def self.mongo_client
+    Mongoid::Clients.default
+  end
+
+  # convenience method for access to zips collection
+  def self.collection
+    self.mongo_client['racers']
+  end
+end
