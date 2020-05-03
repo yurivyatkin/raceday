58c2ee2432b002fbb6df55fc8a74d144ad38b957
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-03 22:12:30 +1200

    Add racers resources to routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  resources :racers
+  root to: 'racers#index'
 
   # Example resource route with options:
   #   resources :products do
