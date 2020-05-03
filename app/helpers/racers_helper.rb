37837d533032e35a07735d29169623dea09f849a
Yuri Vyatkin <yurivyatkin@users.noreply.github.com>
2020-05-03 22:29:53 +1200

    Cleanup values with racers_helper

--- app/helpers/racers_helper.rb
+++ app/helpers/racers_helper.rb
@@ -1,2 +1,8 @@
 module RacersHelper
+  # Our Racer.all method returns a collection of hashes
+  # and not a collection of Racer instances
+  # so that lazy loading can take place
+  def toRacer(value)
+    return value.is_a?(Racer) ? value : Racer.new(value)
+  end
 end
--- app/views/racers/index.json.jbuilder
+++ app/views/racers/index.json.jbuilder
@@ -1,1 +1,5 @@
-json.array! @racers, partial: "racers/racer", as: :racer
+json.array!(@racers) do |racer|
+  racer=toRacer(racer)
+  json.extract! racer, :id, :number, :first_name, :last_name, :gender, :group, :secs
+  json.url racer_url(racer, format: :json)
+end
